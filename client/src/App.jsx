88b9009a77a4d42967a1e2692e3e9464bfd67742
ChamikaRohan <chamika707@gmail.com>
2024-05-27 15:33:04 +0530

    Created Register Page UI

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignupPage from './pages/SignupPage'
+
+export default function App() {
+  return (
+    <div>
+      <SignupPage/>
+    </div>
+  )
+}
---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './SignupPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye,faEyeSlash  } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function SignupPage() {
+    const [fname, setFname] = useState();
+    const [lname, setLname] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [showPassword, setShowPassword] = useState(false);
+  
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+    return (
+    <div className="mainContainer">
+        <div className="subContainer">
+            <label className='topic'>Register</label>
+            <form className='form'>
+                <input placeholder="First name" />
+                <input placeholder="Last name" />
+                <input placeholder="Email" type="email" />
+                <div class="passwordContainer">
+                    <input type={showPassword? "": "password"} class="passwordInput" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                    <button type="button" className="viewPasswordBtn" onClick={togglePasswordVisibility}>{showPassword? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}</button>
+                </div>
+                <button className='btn'>Register</button>
+            </form>
+            <div className='bottomContainer'>
+                <label className='bottomFText' >Already Register?</label>
+                <label className='bottomSText' >Login now</label>
+            </div>
+        </div>
+    </div>
+  )
+}
